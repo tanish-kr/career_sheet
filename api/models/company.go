3f0d90b0a7207e568fe854303fb36465b128f51f
tanish-kr <tora.1986.tatsu@gmail.com>
2021-06-16 19:08:57 +0900

    added Company model

---
+++ api/models/company.go
@@ -0,0 +1,49 @@
+package models
+
+import (
+	"career_sheet/middlewares"
+	"time"
+)
+
+type Company struct {
+	ID             uint      `gorm:"primaryKey" json:"id" uri:"id"`
+	Name           string    `gorm:"not null" json:"name"`
+	EmploymentForm string    `json:"employment_form"`
+	Employees      int       `json:"employees"`
+	StartOn        time.Time `json:"start_on" time_format:"2006-01-02"`
+	EndOn          time.Time `json:"end_on" time_format:"2006-01-02"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+}
+
+func FindCompany(id int) (*Company, error) {
+	var company Company
+	if err := middlewares.DB.Where("id = ?", id).Find(&company).Error; err != nil {
+		return nil, err
+	}
+
+	return &company, nil
+}
+
+func (c *Company) CreateCompany() error {
+	if err := middlewares.DB.Create(&c).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Company) UpdateCompany(data *Company) error {
+	if err := middlewares.DB.Model(&c).Updates(&data).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Company) DeleteCompany() error {
+	if err := middlewares.DB.Delete(&c).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ api/tests/models/company_test.go
@@ -0,0 +1,140 @@
+package models
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"career_sheet/middlewares"
+	"career_sheet/models"
+	"career_sheet/tests"
+)
+
+func TestFindCompany(t *testing.T) {
+	db := tests.GetTestDB()
+	tx := db.Begin()
+	name := "Ruby .inc"
+	employmentForm := "正社員"
+	employees := 10
+	startOn := time.Date(2012, 1, 1, 0, 0, 0, 0, time.Local)
+	endOn := time.Date(2019, 1, 1, 0, 0, 0, 0, time.Local)
+	mock := models.Company{
+		Name:           name,
+		EmploymentForm: employmentForm,
+		Employees:      employees,
+		StartOn:        startOn,
+		EndOn:          endOn,
+	}
+	if err := tx.Create(&mock).Error; err != nil {
+		t.Fatalf("Company create error '%s'", err)
+	}
+	middlewares.DB = tx
+
+	defer tx.Rollback()
+
+	fmt.Println("mock", mock)
+	company, _ := models.FindCompany(int(mock.ID))
+
+	assert.Equal(t, company.ID, mock.ID)
+	assert.Equal(t, company.Name, name)
+	assert.Equal(t, company.EmploymentForm, employmentForm)
+	assert.Equal(t, company.Employees, employees)
+	assert.Equal(t, company.StartOn, startOn)
+	assert.Equal(t, company.EndOn, endOn)
+}
+
+func TestCreateCompany(t *testing.T) {
+	db := tests.GetTestDB()
+	tx := db.Begin()
+	middlewares.DB = tx
+	defer tx.Rollback()
+
+	name := "Ruby .inc"
+	employmentForm := "正社員"
+	employees := 10
+	startOn := time.Date(2012, 1, 1, 0, 0, 0, 0, time.Local)
+	endOn := time.Date(2019, 1, 1, 0, 0, 0, 0, time.Local)
+
+	company := models.Company{
+		Name:           name,
+		EmploymentForm: employmentForm,
+		Employees:      employees,
+		StartOn:        startOn,
+		EndOn:          endOn,
+	}
+
+	if err := company.CreateCompany(); err != nil {
+		t.Errorf("Add Technology error '%s'", err)
+	}
+
+	fmt.Println(company)
+	assert.Greater(t, company.ID, uint(0))
+	assert.Equal(t, company.Name, name)
+	assert.Equal(t, company.EmploymentForm, employmentForm)
+	assert.Equal(t, company.Employees, employees)
+	assert.Equal(t, company.StartOn, startOn)
+	assert.Equal(t, company.EndOn, endOn)
+	assert.NotNil(t, company.CreatedAt)
+	assert.NotNil(t, company.UpdatedAt)
+}
+
+func TestUpdateCompany(t *testing.T) {
+	db := tests.GetTestDB()
+	tx := db.Begin()
+	name := "Ruby .inc"
+	employmentForm := "正社員"
+	employees := 10
+	startOn := time.Date(2012, 1, 1, 0, 0, 0, 0, time.Local)
+	endOn := time.Date(2019, 1, 1, 0, 0, 0, 0, time.Local)
+	company := models.Company{
+		Name:           name,
+		EmploymentForm: employmentForm,
+		Employees:      employees,
+		StartOn:        startOn,
+		EndOn:          endOn,
+	}
+	if err := tx.Create(&company).Error; err != nil {
+		t.Fatalf("Company create error '%s'", err)
+	}
+
+	middlewares.DB = tx
+	defer tx.Rollback()
+
+	updated := company.UpdatedAt
+	if err := company.UpdateCompany(&models.Company{Name: "Go"}); err != nil {
+		t.Fatalf("Technology update error '%s'", err)
+	}
+
+	fmt.Println("Updated company", company)
+	assert.Equal(t, company.Name, "Go")
+	assert.NotEqual(t, company.UpdatedAt, updated)
+}
+
+func TestDeleteCompany(t *testing.T) {
+	db := tests.GetTestDB()
+	tx := db.Begin()
+	name := "Ruby .inc"
+	employmentForm := "正社員"
+	employees := 10
+	startOn := time.Date(2012, 1, 1, 0, 0, 0, 0, time.Local)
+	endOn := time.Date(2019, 1, 1, 0, 0, 0, 0, time.Local)
+	company := models.Company{
+		Name:           name,
+		EmploymentForm: employmentForm,
+		Employees:      employees,
+		StartOn:        startOn,
+		EndOn:          endOn,
+	}
+	if err := tx.Create(&company).Error; err != nil {
+		t.Fatalf("Company create error '%s'", err)
+	}
+
+	middlewares.DB = tx
+	defer tx.Rollback()
+
+	if err := company.DeleteCompany(); err != nil {
+		t.Fatalf("Company delete error '%s'", err)
+	}
+}
