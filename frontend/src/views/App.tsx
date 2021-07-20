deaad0155931336eea29e785050ba1e8a079aa40
tanish-kr <tora.1986.tatsu@gmail.com>
2021-07-20 18:41:35 +0900

    added Profile component

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from "react";
 import "./App.css";
+import "../scss/App.scss";
 import "../locales/config";
 import "bulma/css/bulma.min.css";
 import { Section } from "react-bulma-components";
 import { NavigationComponent } from "./components/Navigation";
+import { Profile } from "./components/Profile";
 
 const App: FC = () => {
   return (
     <>
       <NavigationComponent />
-      <Section></Section>
+      <Section>
+        <Profile />
+      </Section>
     </>
   );
 };
--- frontend/src/views/components/Profile.tsx
+++ frontend/src/views/components/Profile.tsx
@@ -1,7 +1,40 @@
 import React, { FC } from "react";
+import { Level, Heading, Image, Columns, Card, Element } from "react-bulma-components";
 
-const Profile: FC = () => {
-  return <div></div>;
+export const Profile: FC = () => {
+  return (
+    <Columns>
+      <Columns.Column>
+        <Card>
+          <Card.Header.Icon className="avater-header">
+            <Image
+              className="avater"
+              src="https://www.pikpng.com/pngl/m/16-168770_user-iconset-no-profile-picture-icon-circle-clipart.png"
+              size={96}
+              rounded={true}
+            />
+          </Card.Header.Icon>
+          <Card.Content>
+            <Level>
+              <Level.Side align="left">
+                <Heading size={4} renderAs="p">
+                  Name
+                </Heading>
+              </Level.Side>
+              <Level.Side align="right">
+                <Heading size={6} renderAs="p">
+                  Man(Age)
+                </Heading>
+              </Level.Side>
+            </Level>
+            <Element textSize="5" textAlign="left">
+              About
+            </Element>
+          </Card.Content>
+        </Card>
+      </Columns.Column>
+    </Columns>
+  );
 };
 
 export default Profile;
