e16c13d649c4065149498daf7841be57e9484030
tanish-kr <tora.1986.tatsu@gmail.com>
2021-11-03 15:13:29 +0900

    added skill state

---
+++ frontend/src/redux/modules/skills.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+export interface SkillState {
+  name: string;
+  experience: number;
+}
+
+// const initialState = {
+//   name: "",
+//   experience: -1
+// } as SkillState;
+
+const SkillsSlice = createSlice({
+  name: "skills",
+  initialState: [] as SkillState[],
+  reducers: {
+    addSkills(state, action: PayloadAction<SkillState>) {
+      state.push(action.payload)
+    }
+  }
+});
+
+const { addSkills } = SkillsSlice.actions;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const selectSkills = (state: RootState) => state.skills;
+
+export default SkillsSlice.reducer;
--- frontend/src/redux/reducers.ts
+++ frontend/src/redux/reducers.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import translationReducer from "./modules/translations";
 import modalReducer from "./modules/modals";
 import profileReducer from "./modules/profiles";
+import skillsReducer from "./modules/skills";
 
 const rootReducer = combineReducers({
   translations: translationReducer,
   modals: modalReducer,
   profile: profileReducer,
+  skills: skillsReducer,
 });
 
 export default rootReducer;
